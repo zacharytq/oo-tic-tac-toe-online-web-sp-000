f9c746a9ac4de3fe8f761bdccf16303bccd1ccd3
chic-page-6502 <zacharytq@gmail.com>
2019-08-13 23:49:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,8 +15,7 @@ class TicTacToe
   end
 
   def display_board
-    puts " #{@board[0]} |  #{@board[1]} |  #{@board[2]} "
-    puts " #{@board[3]} |  #{@board[4]} |  #{@board[5]} "
-    puts " #{@board[6]} |  #{@board[7]} |  #{@board[8]} "
+    puts " #{@board[0]} |  #{@board[1]} |  #{@board[2]} \n #{@board[3]} |  #{@board[4]} |  #{@board[5]} \n #{@board[6]} |  #{@board[7]} |  #{@board[8]} "
+    
   end
 end
