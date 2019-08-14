1b4396c1c83f7fffa3914f8a84c01f4fcbd787b2
chic-page-6502 <zacharytq@gmail.com>
2019-08-14 00:00:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,5 +38,15 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
 
+  def turn_count
+  counter = 0
+  @board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
 
 end
